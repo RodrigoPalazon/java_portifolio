fd6624a74f29fbdab03e3c727c99f9352efafdb1
Rodrigo Palazon <palazonbatera@gmail.com>
2023-05-11 23:44:49 +0200

    Add class about Primitive data types

--- src/academy/devdojo/maratonajava/introducao/Lesson01_Hello.java
+++ src/academy/devdojo/maratonajava/introducao/Lesson01_Hello.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.introducao;
 /**
  Testing <p>Javadoc</p> comment.
  */
-public class Main {
+public class Lesson01_Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
---
+++ src/academy/devdojo/maratonajava/introducao/Lesson02_PrimitiveTypes.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson02_PrimitiveTypes {
+    public static void main(String[] args) {
+//        int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        long longTypeNumber = 10000;
+        double salaryDoubleType = 2000;
+        float salaryFloatType = 2500;
+        byte ageByte = 127;
+        short ageShort = 8;
+        boolean booleanTrue = true;
+        boolean booleanFalse = false;
+        char charType = 65;
+        char uniCodeValue = '\u0040';
+
+        System.out.println("The age is: " + age);
+        System.out.println(booleanTrue);
+        System.out.println("Character: " + charType);
+        System.out.println("Character: " + uniCodeValue);
+    }
+}
