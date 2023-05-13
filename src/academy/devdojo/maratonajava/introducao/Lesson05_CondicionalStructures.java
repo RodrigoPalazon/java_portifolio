cd67148c03c0e1242e199c7d430ecd40c9068c79
Rodrigo Palazon <palazonbatera@gmail.com>
2023-05-13 19:37:57 +0200

    Add Condicional Structure (part 1.)

---
+++ src/academy/devdojo/maratonajava/introducao/Lesson05_CondicionalStructures.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson05_CondicionalStructures {
+    public static void main(String[] args) {
+        int age_example1 = 35;
+        int age_ternaryOperator = 18;
+        boolean allowedToDrink = (age_example1 >= 18 && age_ternaryOperator >= 18);
+
+        if(!allowedToDrink) {
+            System.out.println("Not allowed to drink.");
+        }else{
+            System.out.println("Allowed to drink.");
+        }
+        System.out.println("Thanks for checking your age.");
+
+
+        //Ternary Operator
+
+        String message = allowedToDrink ? "Allowed to buy alcoholic drink.":"NOT Allowed to buy alcoholic drink.";
+        System.out.println(message);
+    }
+}
