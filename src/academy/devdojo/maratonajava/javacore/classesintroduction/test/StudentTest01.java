adde2151e789fbe3daad70544025189791214bff
Rodrigo Palazon <palazonbatera@gmail.com>
2023-05-17 22:30:58 +0200

    Create new package about Object-oriented programming

---
+++ src/academy/devdojo/maratonajava/javacore/classesintroduction/test/StudentTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.classesintroduction.test;
+
+
+import academy.devdojo.maratonajava.javacore.classesintroduction.domain.Student;
+
+public class StudentTest01 {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.name = "Rodrigo";
+        student.age = 35;
+        student.gender = 'M';
+
+        System.out.println(student.name);
+        System.out.println(student.age);
+        System.out.println(student.gender);
+    }
+}
