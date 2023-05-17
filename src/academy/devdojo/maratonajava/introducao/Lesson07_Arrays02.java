afa1c273f4e13b49b9ccfa0032797a96ad448962
Rodrigo Palazon <palazonbatera@gmail.com>
2023-05-17 17:23:15 +0200

    Display default values for different data types within an array

---
+++ src/academy/devdojo/maratonajava/introducao/Lesson07_Arrays02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson07_Arrays02 {
+    public static void main(String[] args) {
+//        byte, short, int, long, float e double ... 0
+//        char '\u0000'
+//        boolean false
+//        String null
+
+        String[] names = new String[3];
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+    }
+}
