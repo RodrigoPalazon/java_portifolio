96412b61c3e8edfed78c89f77daf9a8fd1aba25a
Rodrigo Palazon <palazonbatera@gmail.com>
2023-05-17 17:15:54 +0200

    Introduction about Arrays

---
+++ src/academy/devdojo/maratonajava/introducao/Lesson07_Arrays01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson07_Arrays01 {
+    public static void main(String[] args) {
+        int[] ages = new int[3];
+
+        ages[0] = 15;
+        ages[1] = 56;
+        ages[2] = 41;
+
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+    }
+}
