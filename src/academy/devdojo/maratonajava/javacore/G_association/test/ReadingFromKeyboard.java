37f6264fe1c775affe4b0fa30cd65a54bb2b4f87
Rodrigo Palazon <palazonbatera@gmail.com>
2023-05-25 17:45:37 +0200

    Add Scanner class to read user's input

---
+++ src/academy/devdojo/maratonajava/javacore/G_association/test/ReadingFromKeyboard.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.G_association.test;
+
+import java.util.Scanner;
+
+public class ReadingFromKeyboard {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Type your name:");
+        String name = input.nextLine();
+        System.out.println("Type your age:");
+        int age = input.nextInt();
+        System.out.println("Type M or F to your gender:");
+        char gender = input.next().charAt(0);
+        System.out.println("-------");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+
+    }
+
+}
