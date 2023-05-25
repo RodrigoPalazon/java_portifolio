a5d6da7deb06dc531b4fdb772386df9ce79918c1
Rodrigo Palazon <palazonbatera@gmail.com>
2023-05-25 11:09:01 +0200

    Add Static Initialization Blocks

--- src/academy/devdojo/maratonajava/javacore/F_staticModifiers/domain/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/F_staticModifiers/domain/Anime.java
@@ -1,26 +1,39 @@
-package academy.devdojo.maratonajava.javacore.E_initializationBlocks.domain;
+package academy.devdojo.maratonajava.javacore.F_staticModifiers.domain;
 
 public class Anime {
     private String name;
-    private int[] episodes;
+    private static int[] episodes;
 
 //    Order of compiling
+//      0- Initialization Block is executed when the JVM load the class
 //      1- Allocate memory space for  object
 //      2- Each class attribute is created and initialized with default or set values
 //      3- Initialization block is executed
 //      4- Constructor is executed
 
 //     Instance Block Initialization
-    {
-        System.out.println("Within the Initialization Block");
+    static {
+        System.out.println("Within the Initialization STATIC Block - 01");
         episodes = new int [100];
         for(int i=0; i<episodes.length; i++) {
             episodes[i] = i + 1;
         }
     }
 
+    static {
+        System.out.println("Within the Initialization STATIC Block - 02");
+    }
+
+    static {
+        System.out.println("Within the Initialization STATIC Block - 03");
+    }
+
+    {
+        System.out.println("NOT Static Block");
+    }
+
     public Anime() {
-        for (int episode:this.episodes) {
+        for (int episode: Anime.episodes) {
             System.out.print(episode + " ");
         }
         System.out.println();
---
+++ src/academy/devdojo/maratonajava/javacore/F_staticModifiers/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.F_staticModifiers.test;
+
+import academy.devdojo.maratonajava.javacore.F_staticModifiers.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime_01 = new Anime();
+        Anime anime_02 = new Anime();
+        Anime anime_03 = new Anime();
+    }
+}
