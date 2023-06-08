2152507864bdc67181f3911bde858818a7ceb2f4
Rodrigo Palazon <palazonbatera@gmail.com>
2023-06-08 15:39:26 +0200

    Exception part.8 - Mult catch in line

--- src/academy/devdojo/maratonajava/javacore/O_exception/runtime/test/RunTimeExceptionTest04.java
+++ src/academy/devdojo/maratonajava/javacore/O_exception/runtime/test/RunTimeExceptionTest04.java
@@ -5,27 +5,34 @@ import java.sql.SQLException;
 
 public class RunTimeExceptionTest04 {
     public static void main(String[] args) {
-         try{
-             throw new RuntimeException();
-         }catch (ArrayIndexOutOfBoundsException e){
-             System.out.println("Within ArrayIndexOutOfBoundsException");
-         }catch (IndexOutOfBoundsException e){
-             System.out.println("Within IndexOutOfBoundsException");
-         }catch (IllegalArgumentException e){
-             System.out.println("Within IllegalArgumentException");
-         }catch (ArithmeticException e){
-             System.out.println("Within ArithmeticException");
-         }catch (RuntimeException e){
-             System.out.println("Within ArrayIndexOutOfBoundsException");
-         }
+        try {
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Within ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Within IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Within IllegalArgumentException");
+        } catch (ArithmeticException e) {
+            System.out.println("Within ArithmeticException");
+        } catch (RuntimeException e) {
+            System.out.println("Within ArrayIndexOutOfBoundsException");
+        }
 
-         try {
-             maybeLaunchException();
-         }catch (SQLException e){
+//         try {
+//             maybeLaunchException();
+//         }catch (SQLException e){
+//
+//         }catch (FileNotFoundException e){
+//
+//         }
 
-         }catch (FileNotFoundException e){
-
-         }
+//        Refactoring the scope above:
+        try {
+            maybeLaunchException();
+        } catch (SQLException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void maybeLaunchException() throws SQLException, FileNotFoundException {
