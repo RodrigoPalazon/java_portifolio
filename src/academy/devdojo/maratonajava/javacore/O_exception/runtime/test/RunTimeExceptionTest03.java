41f2aa4b523cbb9b91f9e698fe78f90affaeb50d
Rodrigo Palazon <palazonbatera@gmail.com>
2023-06-08 13:19:45 +0200

    Exceptions part.6 - Finally block

---
+++ src/academy/devdojo/maratonajava/javacore/O_exception/runtime/test/RunTimeExceptionTest03.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.O_exception.runtime.test;
+
+public class RunTimeExceptionTest03 {
+    public static void main(String[] args) {
+        openConection();
+        openConection2();
+    }
+
+    private static String openConection() {
+        try{
+            System.out.println("Opening a File.");
+            System.out.println("Writing data in a File.");
+            return "Opened Connection";
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("closing feature released by OS");
+        }
+        return null;
+    }
+
+//    Quite specific, but possible use is...
+    private static String openConection2() {
+        try{
+            System.out.println("Opening a File.");
+            System.out.println("Writing data in a File.");
+            throw new RuntimeException();
+        }finally {
+            System.out.println("closing feature released by OS");
+        }
+        return null;
+    }
+}
