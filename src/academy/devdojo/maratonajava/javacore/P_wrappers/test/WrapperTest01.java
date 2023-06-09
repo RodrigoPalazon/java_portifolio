f7ead21a61c9e22d97d9b97cd69781be3179c53c
Rodrigo Palazon <palazonbatera@gmail.com>
2023-06-09 11:05:04 +0200

    Wrappers part.1

---
+++ src/academy/devdojo/maratonajava/javacore/P_wrappers/test/WrapperTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.P_wrappers.test;
+
+import java.util.List;
+
+public class WrapperTest01 {
+//    primitive data types:
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+        booleanW.booleanValue();
+    }
+}
+
