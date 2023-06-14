b7b6f6650ac5046169caed2365e6745e22d9f736
Rodrigo Palazon <palazonbatera@gmail.com>
2023-06-14 11:34:31 +0200

    Internacionalization of currency with Locale

--- src/academy/devdojo/maratonajava/javacore/S_formatting/test/NumberFormatTest02.java
+++ src/academy/devdojo/maratonajava/javacore/S_formatting/test/NumberFormatTest02.java
@@ -4,26 +4,26 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
         Locale localeBR = new Locale("pt","BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
         NumberFormat[] numberFormatArray = new NumberFormat[4];
-        numberFormatArray[0] = NumberFormat.getInstance();
-        numberFormatArray[1] = NumberFormat.getInstance(localeBR);
-        numberFormatArray[2] = NumberFormat.getInstance(localeJP);
-        numberFormatArray[3] = NumberFormat.getInstance(localeIT);
+        numberFormatArray[0] = NumberFormat.getCurrencyInstance();
+        numberFormatArray[1] = NumberFormat.getCurrencyInstance(localeBR);
+        numberFormatArray[2] = NumberFormat.getCurrencyInstance(localeJP);
+        numberFormatArray[3] = NumberFormat.getCurrencyInstance(localeIT);
         double value = 100_000_000.2134;
 
         for (NumberFormat numberFormat : numberFormatArray) {
-            numberFormat.setMaximumFractionDigits(2);
+            System.out.println(numberFormat.getMaximumFractionDigits());
             System.out.println(numberFormat.format(value));
         }
-//        String valueString = "1_000.2130"; format not valid;
-        String valueString = "1000.2130";
+
+        String valueString = "￥100,000";
         try {
-            System.out.println(numberFormatArray[0].parse(valueString));
+            System.out.println(numberFormatArray[2].parse(valueString));
         }catch (ParseException e){
             e.printStackTrace();
         }
