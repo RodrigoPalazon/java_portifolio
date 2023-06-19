e682ce7122b3c476232237f8b35d027b590e98ce
Rodrigo Palazon <palazonbatera@gmail.com>
2023-06-19 21:24:00 +0200

    LocalDate

---
+++ src/academy/devdojo/maratonajava/javacore/R_dates/test/LocalDateTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.R_dates.test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+        System.out.println(Month.JANUARY.getValue());
+        LocalDate date = LocalDate.of(2023, Month.JANUARY, 27);
+        System.out.println(date.getDayOfWeek());
+        System.out.println(date.getDayOfMonth());
+        System.out.println(date.lengthOfMonth());
+        System.out.println(date.isLeapYear());
+        System.out.println(date);
+    }
+}
