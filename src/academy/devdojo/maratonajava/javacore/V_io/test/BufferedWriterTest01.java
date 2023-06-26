ea7c74e44b3032d19d6a6c016f80dccb60e67b62
Rodrigo Palazon <palazonbatera@gmail.com>
2023-06-26 18:36:45 +0200

    I/O File class part.05 - BufferedReader

--- src/academy/devdojo/maratonajava/javacore/V_io/test/BufferedWriterTest01.java
+++ src/academy/devdojo/maratonajava/javacore/V_io/test/BufferedWriterTest01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try(FileWriter fw= new FileWriter(file,true);
-            BufferedWriter br = new BufferedWriter(fw);) {
-            br.write("The DevDojo is the best brazilian Java course. This goes in the second line of the text.");
-            br.newLine();
-            br .flush();
+            BufferedWriter bw = new BufferedWriter(fw);) {
+            bw.write("The DevDojo is the best brazilian Java course. This goes in the second line of the text.");
+            bw.newLine();
+            bw .flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
