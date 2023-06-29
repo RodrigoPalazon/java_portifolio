ea1f6519b5c192fe84fe3ece525f4c15436a0b1f
Rodrigo Palazon <palazonbatera@gmail.com>
2023-06-29 17:12:30 +0200

    NIO part.10 - DirectoryStream

---
+++ src/academy/devdojo/maratonajava/javacore/W_nio/test/DirectoryStreamTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.W_nio.test;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryStreamTest01 {
+    public static void main(String[] args) {
+        Path dir = Paths.get(".");
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)){
+            for (Path path: stream) {
+                System.out.println(path.getFileName());
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
