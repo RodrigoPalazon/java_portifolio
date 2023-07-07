e510377c75b5f36c8803b6bc0dced4d4a26f6a81
Rodrigo Palazon <palazonbatera@gmail.com>
2023-07-07 16:32:27 +0200

    List part.02

--- src/academy/devdojo/maratonajava/javacore/Y_collections/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Y_collections/test/ListTest01.java
@@ -5,21 +5,33 @@ import java.util.List;
 
 public class ListTest01 {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>(16);
-        names.add("Rodrigo");
-        names.add("Palazon");
+        List<String> names_01 = new ArrayList<>(16);
+        List<String> names_02 = new ArrayList<>(16);
+        names_01.add("Rodrigo");
+        names_01.add("Palazon");
+        names_01.remove("Palazon");
+        names_02.add("Yolanda");
+        names_02.add("Abravanel");
 
-        for (String name:names) {
+        names_01.addAll(names_02);
+
+        for (String name:names_01) {
             System.out.println(name);
         }
 
-        names.add("Yolanda");
+        names_01.add("Yolanda");
         System.out.println("-----------------");
 
-        for (int i=0; i< names.size(); i++){
-            System.out.println(names.get(i));
+        int size = names_01.size();
+        for (int i=0; i < size; i++){
+            System.out.println(names_01.get(i));
         }
 
-        System.out.println(names);
+        System.out.println(names_01);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(12);
+
+        System.out.println(numbers);
     }
 }
