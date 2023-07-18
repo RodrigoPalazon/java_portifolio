987eb287aca87da58ed3c5006e7e11bb8a19f542
Rodrigo Palazon <palazonbatera@gmail.com>
2023-07-18 11:48:23 +0200

    Navigable, TreeSet part.02

--- src/academy/devdojo/maratonajava/javacore/Y_collections/test/NavigableSetTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Y_collections/test/NavigableSetTest01.java
@@ -14,6 +14,14 @@ class SmarthphoneBrandComparator implements Comparator<Smartphone> {
         return o1.getBrand().compareTo(o2.getBrand());
     }
 }
+
+class ProgrammingLanguageVersionComparator implements Comparator<ProgrammingLanguage> {
+    @Override
+    public int compare(ProgrammingLanguage o1, ProgrammingLanguage o2) {
+        return Double.compare(o1.getVersion(), o2.getVersion());
+    }
+}
+
 public class NavigableSetTest01 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmarthphoneBrandComparator());
@@ -21,17 +29,22 @@ public class NavigableSetTest01 {
         set.add(smartphone);
         System.out.println(set);
 
-        NavigableSet<ProgrammingLanguage> programmingLanguages = new TreeSet<>();
-        programmingLanguages.add(new ProgrammingLanguage(06L,"Java",18));
-        programmingLanguages.add(new ProgrammingLanguage(02L,"JavaScript",6));
-        programmingLanguages.add(new ProgrammingLanguage(07L,"Python", 3));
-        programmingLanguages.add(new ProgrammingLanguage(01L,"Kotlin",1.1));
-        programmingLanguages.add(new ProgrammingLanguage(03L,"C",0.01));
-        programmingLanguages.add(new ProgrammingLanguage(04L,"Go", 7.01));
-        programmingLanguages.add(new ProgrammingLanguage(04L,"Go", 7.01));
+        NavigableSet<ProgrammingLanguage> programmingLanguages = new TreeSet<>(new ProgrammingLanguageVersionComparator());
+        programmingLanguages.add(new ProgrammingLanguage(06L,"Java",18,0));
+        programmingLanguages.add(new ProgrammingLanguage(02L,"JavaScript",6, 2));
+        programmingLanguages.add(new ProgrammingLanguage(07L,"Python", 3, 4));
+        programmingLanguages.add(new ProgrammingLanguage(01L,"Kotlin",1.1, 5));
+        programmingLanguages.add(new ProgrammingLanguage(03L,"C",0.01, 2));
+        programmingLanguages.add(new ProgrammingLanguage(04L,"Go", 7.01, 32));
+        programmingLanguages.add(new ProgrammingLanguage(04L,"Go", 7.01, 7));
 
-        for (ProgrammingLanguage programmingLanguage : programmingLanguages) {
+        for (ProgrammingLanguage programmingLanguage : programmingLanguages.descendingSet()) {
             System.out.println(programmingLanguage);
         }
+
+        //lower <
+        //floor <=
+        //higher >
+        //ceiling >=
     }
 }
