dc7fb68470e6830151170e7933e29501e7832fbf
Rodrigo Palazon <palazonbatera@gmail.com>
2023-08-20 21:28:28 +0200

    Generics part.03 - Wildcard part.02

--- src/academy/devdojo/maratonajava/javacore/Z_generics/test/WildcardTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Z_generics/test/WildcardTest02.java
@@ -1,18 +1,27 @@
 package academy.devdojo.maratonajava.javacore.Z_generics.test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WildcardTest02 {
     public static void main(String[] args) {
         List<Dog> dogs = List.of(new Dog(), new Dog());
         List<Cat> cats = List.of(new Cat(), new Cat());
+        printAppointment(dogs);
+        printAppointment(cats);
+        List<Animal> animals = new ArrayList<>();
+        printAppointmentAnimal(animals);
     }
 
-//    Type erasure error
-    private static void printAppointment(List<Animal> animals){
+//    Type erasure ?
+    private static void printAppointment(List<? extends  Animal> animals){
         for (Animal animal: animals) {
             animal.appointment();
         }
+    }
+
+    private static void printAppointmentAnimal (List<? super Animal> animals) {
         animals.add(new Dog());
+        animals.add(new Cat());
     }
 }
