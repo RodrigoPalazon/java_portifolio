6eb22e721ce92f751795a21248e607a63fedfdab
Rodrigo Palazon <palazonbatera@gmail.com>
2023-09-09 23:25:05 +0200

    Lambda part.01/02 - Consumer

--- src/academy/devdojo/maratonajava/javacore/ZZB_behavior/intefaces/CarPredicate.java
+++ src/academy/devdojo/maratonajava/javacore/ZZB_behavior/intefaces/CarPredicate.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.ZZB_behavior.intefaces;
 
 import academy.devdojo.maratonajava.javacore.ZZB_behavior.domain.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
     boolean test(Car car);
 }
